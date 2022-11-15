4c850d6e48f591b37404b5654d35bc35d13cb5e7
Cem KARAKUŞ <karakuscemenes@gmail.com>
2022-11-15 21:10:51 +0300

    project setup

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import logo from '../Assets/logo.svg';
+
+export default function Header() {
+    return (
+        <div className="header">
+            <img src={logo} alt=""></img>
+        </div>
+    )
+}
