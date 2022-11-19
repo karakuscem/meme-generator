410408f925d9ab14e6c3628428b262d54beee758
Cem KARAKUŞ <karakuscemenes@gmail.com>
2022-11-19 21:19:28 +0300

    done

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Meme Generator App
 
 Simple meme generator to study react.
+
+## Live
+
+[Click]()
+
+### What's Learned
+
+-Creating reusable components.
+
+-Using Props.
+
+-Using array.map method with Props.
+
+-Passing objects as Props.
+
+-Making API Calls
+
+-Conditional Rendering
+
+-Dynamic Styles
+
+-Using Forms
--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -1,13 +1,49 @@
+import React from "react"
+
 export default function Inputs() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImg: "http://i.imgflip.com/1bij.jpg",
+    })
+
+    const [allMemeImgs, setAllMemeImgs] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                setAllMemeImgs(response.data.memes)
+            })
+    }, [])
+
+    function get_memes() {
+        const randomNum = Math.floor(Math.random() * allMemeImgs.length)
+        const randomMemeImg = allMemeImgs[randomNum].url
+        setMeme({...meme, randomImg: randomMemeImg})
+    }
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
     return (
-        <form className='inputs'>
+        <div className='inputs'>
             <div className='inputs--top'>
-                <input type='text' placeholder='Top Text' className='inputs--top--writing'></input>
-                <input type='text' placeholder='Bottom Text' className='inputs--bottom--writing'></input>
+                <input type='text' placeholder='Top Text' className='inputs--top--writing' name="topText" value={meme.topText} onChange={handleChange}></input>
+                <input type='text' placeholder='Bottom Text' className='inputs--bottom--writing' name="bottomText" value={meme.bottomText} onChange={handleChange}></input>
             </div>
             <div className='inputs--bottom'>
-                <button className='inputs--button'>Generate new meme image 🖼️</button>
+                <button className='inputs--button' onClick={get_memes}>Generate new meme image 🖼️</button>
             </div>
-        </form>
+            <div className="meme">
+                <img src={meme.randomImg} className='meme--image' alt=""></img>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </div>
     )
 }
